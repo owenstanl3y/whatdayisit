ed0244c65b7af4de570856b988a03887f74ac3ef
ChiquitaV2 <46801600+owenstanl3y@users.noreply.github.com>
2021-05-03 16:32:42 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,18 @@ var data = fs.readFileSync('dates.json', 'utf8');
 var json = JSON.parse(data);
 
 
-let date = new Date();
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { today: json[date.getMonth() + "-" + date.getDate()] , tomorrow: json[date.getMonth() + "-" + (date.getDate() + 1)] });
+  res.render('index', { today: json[getDate()] , tomorrow: json[getTomorrow()] });
 });
 
+function getDate() {
+  return new Date().getMonth() + "-" + new Date().getDate();
+}
+
+function getTomorrow() {
+  return new Date().getMonth() + "-" + (new Date().getDate() + 1);
+}
+
 module.exports = router;
+
